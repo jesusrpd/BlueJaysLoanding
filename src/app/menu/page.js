0e13f0dd4eaee689e44e3c5c23fbdec4ff7897ba
jesusrpd <jesusrodrigo881@gmail.com>
2023-07-27 18:26:38 -0600

    header of site finish

---
+++ src/app/menu/page.js
@@ -0,0 +1,5 @@
+export default function Menu(){
+    return(
+        <h3>Menú</h3>
+    )
+} 
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,10 +8,12 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'main': 'url(/img/bg-main.png)'
       },
+      colors:{
+        'bluejays':'#0073B2',
+        'yellowjays': '#E8A800'
+      }
     },
   },
   plugins: [],
