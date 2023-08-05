cd5df3b2c5b19b27cc081d959f1e2fe2a83b4aa8
jesusrpd <jesusrodrigo881@gmail.com>
2023-08-04 23:57:57 -0600

    ajustes a la version tablet del sitio

--- src/app/page.js
+++ src/app/page.js
@@ -6,26 +6,26 @@ export default function Home() {
   return (
     <>
       <main className='w-full h-screen bg-main md:bg-main-tablet bg-cover md:bg-no-repeat flex items-center relative bg-grayjays'>
-        <nav className='flex w-full justify-between px-4 pt-4 absolute top-0 left-0'>
+        <nav className='flex w-full justify-between items-center px-4 pt-4 absolute top-0 left-0'>
           <Image src="/img/logotipo.png" alt="logotipo Buejay's" width={59} height={60}/>
           <div className='nav-lines-container cursor-pointer h-fit md:hidden'>
             <div className='w-8 h-1 bg-white rounded-2xl my-2'></div>
             <div className='w-8 h-1 bg-white rounded-2xl my-2'></div>
             <div className='w-8 h-1 bg-white rounded-2xl my-2'></div>
           </div>
-          <ul className='flex'>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
+          <ul className='hidden md:flex'>
+            <li className='mx-3 text-white font-bold text-lg'>Home</li>
+            <li className='mx-3 text-white font-bold text-lg'>About BlueJays</li>
+            <li className='mx-3 text-white font-bold text-lg'>Menu</li>
+            <li className='mx-3 text-white font-bold text-lg'>Horary</li>
+            <li className='mx-3 text-white font-bold text-lg'>Activities</li>
+            <li className='mx-3 text-white font-bold text-lg'>Location</li>
           </ul>
         </nav>
         <div className='w-full flex flex-col items-center'>
-          <h1 className='text-white font-extrabold text-3xl my-2'>BLUE JAYS</h1>
-          <h3 className='text-white font-extrabold text-sm my-2'>Bar & Restaurant Family</h3>
-          <Link href="/menu" className='border-yellowjays border-2 px-12 py-2 text-yellowjays font-extrabold text-sm my-2'>See menu</Link>
+          <h1 className='text-white font-extrabold text-3xl my-2 md:my-5 md:text-6xl'>BLUE JAYS</h1>
+          <h3 className='text-white font-extrabold text-sm my-2 md:my-5 md:text-2xl'>Bar & Restaurant Family</h3>
+          <Link href="/menu" className='border-yellowjays border-2 px-12 py-2 text-yellowjays font-extrabold text-sm my-2 md:text-lg md:my-5'>See menu</Link>
         </div>
         <Image src="/img/food/aritos.png" alt='img of food aritos' width={294.7} height={314.22} className='absolute bottom-0 negative-position'/>
       </main>
