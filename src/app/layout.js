1d22146dac6b5efdd855e11bb2d1ed500a57422e
jesusrpd <jesusrodrigo881@gmail.com>
2023-08-12 12:28:32 -0600

    metadatos

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,6 +6,7 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata = {
   title: 'Blue Jay\'s',
   description: 'Bring your family or friends to enjoy our burgers, tacos, octopus cocktails, drinks prepared by professional bartenders.',
+  // metadata: 
 }
 
 export default function RootLayout({ children }) {
@@ -16,7 +17,7 @@ export default function RootLayout({ children }) {
         <meta property='og:description' content='Bring your family or friends to enjoy our burgers, tacos, octopus cocktails, drinks prepared by professional bartenders.'/>
         <meta property='og:image' content='../../public/img/logotipo.webp'/>
         <meta name='description' content='Bring your family or friends to enjoy our burgers, tacos, octopus cocktails, drinks prepared by professional bartenders.'/>
-        <meta name='keywords' content='food, bar, restaurant, family, hamburger, tacos, cocktails, cakes, beverages'/>
+        <meta name='keywords' content="food, bar, restaurant, family, hamburger, tacos, cocktails, cakes, beverages, blue jays, bluejays, bluejay's, bluejay's restaurant"/>
         <meta name='robots' content='index,follow'/>
       </head>
       <body className={inter.className}>{children}</body>
